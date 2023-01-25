955362319ab6d6601f365d5342d179712d8a4775
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-01-25 06:12:41 +0100

    Remove NextAuth

--- src/components/StaticMap.tsx
+++ src/components/StaticMap.tsx
@@ -1,16 +1,3 @@
-import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import {
-  ComponentPropsWithRef,
-  ComponentPropsWithoutRef,
-  forwardRef,
-  PropsWithChildren,
-  useState,
-  useRef,
-  ChangeEvent,
-} from "react";
-
 const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_KEY;
 
 export const StaticMap = ({ longitude = 0, latitude = 0, zoom = 16 }) => {
